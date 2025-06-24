215fbb008ecd56993dd1381ee0a6d4f730e5fdb8
SOYHACHE <113484850+SOYHACHE@users.noreply.github.com>
2025-06-24 11:22:15 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-prueba
+const typed = new Typed(".multiple-text", {
+    strings: ["Desarrollador Web", "Diseñador Gráfico", "Freelancer", "Analista de Sistemas", "Flojo y Borracho ^_^"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop: true
+  });
