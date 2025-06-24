f18f7ceceda7fc2238878f18173fdfd3ff2b5e6a
SOYHACHE <113484850+SOYHACHE@users.noreply.github.com>
2025-06-24 11:33:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".multiple-text", {
+    strings: ["Desarrollador Web", "Diseñador Gráfico", "Freelancer", "Analista de Sistemas", "Flojo y Borracho ^_^"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop: true
+  });
